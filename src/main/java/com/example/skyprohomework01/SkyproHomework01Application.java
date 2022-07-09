f447bf04dd0b10650b18cc8c03c33de489677013
Telion <www.neon@mail.ru>
2022-07-09 23:01:59 +0500

    Initial commit

---
+++ src/main/java/com/example/skyprohomework01/SkyproHomework01Application.java
@@ -0,0 +1,13 @@
+package com.example.skyprohomework01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproHomework01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyproHomework01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/skyprohomework01/SkyproHomework01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skyprohomework01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproHomework01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
